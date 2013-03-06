339ed05ef04333819e6778af5a6f96935c96ac94
lawsons <lawsons@lawsons-HP-Z800-Workstation>
2013-03-06 13:34:42 -0500

    Additional tests.

--- spec/SpubSubSpec.js
+++ spec/SpubSubSpec.js
@@ -5,36 +5,87 @@ describe("SpubSub", function() {
 		spubSub = SpubSub();
 	});
 
-	it("spubSub should not be null", function() {
+	it("should not be null", function() {
 		expect(spubSub).not.toBeNull();
 	});
-	
-	it("spubSub should be defined", function() {
+
+	it("should be defined", function() {
 		expect(spubSub).toBeDefined();
 	});
 
-	it("spubSub subscription should work", function() {
-		var keyUnderTest = "testEvent";
+	it("can subscribe with one key", function() {
+		var keyUnderTest = "key";
 		var fired = false;
-		
+
 		spubSub.subscribe({
-			key: keyUnderTest,
-			fn: function(key, msg) {
+			key : keyUnderTest,
+			fn : function(key, msg) {
 				fired = true;
 			}
 		});
-		
-		spubSub.store(keyUnderTest, true);
-		
-		waitsFor(function() {
-            return fired;
-        }, 'Event listener never fired', 1000);
 
-        runs(function() {
-        	expect(fired).toEqual(true);
-        });
-		
-				
+		spubSub.store(keyUnderTest, true);
+
+		waitsFor(function() {
+			return fired;
+		}, 'Event listener never fired', 1000);
+
+		runs(function() {
+			expect(fired).toEqual(true);
+		});
+
 	});
 	
+	it("can subscribe with many keys in one subscribe", function() {
+		var keysUnderTest = [ "key1", "key2", "key3", "key4", "key5" ];
+		var fired = 0;
+
+		spubSub.subscribe({
+			key : keysUnderTest,
+			fn : function(key, msg) {
+				fired++;
+			}
+		});
+		
+		keysUnderTest.forEach(function(key) {
+			spubSub.store(key, true);
+		});
+
+		waitsFor(function() {
+			return fired === keysUnderTest.length;
+		}, 'Event listener never fired', 1000);
+
+		runs(function() {
+			expect(fired).toEqual(keysUnderTest.length);
+		});
+
+	});
+	
+	it("can subscribe with many keys in many subscribe", function() {
+		var keysUnderTest = [ "key1", "key2", "key3", "key4", "key5" ];
+		var fired = 0;
+
+		keysUnderTest.forEach(function(key) {
+			spubSub.subscribe({
+				key : key,
+				fn : function(key, msg) {
+					fired++;
+				}
+			});
+		});
+		
+		keysUnderTest.forEach(function(key) {
+			spubSub.store(key, true);
+		});
+
+		waitsFor(function() {
+			return fired === keysUnderTest.length;
+		}, 'Event listener never fired', 1000);
+
+		runs(function() {
+			expect(fired).toEqual(keysUnderTest.length);
+		});
+
+	});
+
 });
